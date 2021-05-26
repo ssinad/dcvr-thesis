59306fe6321f8dfc9d8c656a9e5113b90824260d
ssinad <27953063+ssinad@users.noreply.github.com>
2021-05-25 22:37:41 -0600

    Clean up orienteering.h

--- src/orienteering.h
+++ src/orienteering.h
@@ -6,11 +6,32 @@
 #include "Penalties.h"
 #include "Path.h"
 
-Path get_path(const Arborescence &arb_T, const Node &root_node, const Node &t, bool triangle_inequality = true);
+Path get_path(  
+                const Arborescence &arb_T,
+                const Node &root_node,
+                const Node &t,
+                bool triangle_inequality = true
+            );
 distance_t edge_cost(const Arborescence &arb_T, const Matrix &costs);
-void binary_search(Arborescence &a1, Arborescence &a2, const Vertices &vertices, const Matrix &costs, const Penalties& penalties, int num_nodes, const Node &t, distance_t distance_limit_D);
+void binary_search(
+                    Arborescence &a1,
+                    Arborescence &a2,
+                    const Vertices &vertices,
+                    const Matrix &costs,
+                    const Penalties& penalties,
+                    int num_nodes,
+                    const Node &t,
+                    distance_t distance_limit_D
+                );
 Path cut_path(const Path &p_i, const Matrix &costs, const Rewards &rewards, const Node &root_node, distance_t distance_limit_D);
-std::pair<Node, Path> orienteering(const Vertices &vertices, const Node &root_node, const Matrix &distances, const Rewards &rewards, distance_t distance_limit_D, std::unordered_map<Node, penalty_t>& upper_bounds);
+std::pair<Node, Path> orienteering(
+                                    const Vertices &vertices,
+                                    const Node &root_node,
+                                    const Matrix &distances,
+                                    const Rewards &rewards,
+                                    distance_t distance_limit_D,
+                                    std::unordered_map<Node, penalty_t>& upper_bounds
+                                );
 penalty_t get_path_reward(const Path &p, const Rewards &rewards);
 distance_t get_path_distance(const Path &p, const Matrix &distances, const Node& root_node);
 #endif
